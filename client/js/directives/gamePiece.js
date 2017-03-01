e0e03a3812aedbc67fd27102c18a35fb09c94e09
brian ally <brian@zijn-digital.com>
2017-03-01 17:39:50 -0500

    updated directives

--- client/js/directives/gamePiece.js
+++ client/js/directives/gamePiece.js
@@ -6,12 +6,57 @@ angular
 
 function gamePiece(squarePosition) {
 
-	function gamePieceCtrl() {
+	function gamePieceCtrl($scope, $element, $attrs) {
 
 	}
 
 	function gamePieceLink(scope, element, attributes, controller) {
+		var el   = element[0];
 
+		el.draggable = true;
+		el.dataset.king = false;
+
+		function dragStart(evt) {
+			let parentId     = el.parentNode.id;
+			let sourceSquare = document.getElementById(parentId);
+			let data         = `{"gamePieceId": "${this.id}", "sourceId": "${parentId}"}`;
+
+
+
+			// is move allowed from here?
+			let direction = el.classList.contains("king") ? 0 : el.dataset.dir;
+			let neighbours = squarePosition.getNeighboursFromId(parentId, direction);
+			//console.log(neighbours);
+
+			let hasMove = false;
+
+			neighbours.forEach(function(sq) {
+
+			});
+
+			// need to find occupied neighbours
+			// if occupied, by whom?
+			// if occupied, is opposite sq available?
+			// 
+
+
+
+
+
+			evt.dataTransfer.effectAllowed = "move";
+			evt.dataTransfer.setData("text/plain", data);
+
+			this.classList.add("dragging");
+			return false;
+		}
+
+		function dragEnd(evt) {
+			this.classList.remove("dragging");
+			return false;			
+		}
+
+		el.addEventListener("dragstart", dragStart, false);
+		el.addEventListener("dragend", dragEnd, false);
 	}
 
 	return {
--- client/js/directives/playingSquare.js
+++ client/js/directives/playingSquare.js
@@ -6,12 +6,77 @@ angular
 
 function playingSquare(squarePosition) {
 
-	function playingSquareCtrl() {
+	function playingSquareCtrl($scope, $element, $attrs) {
 
 	}
 
 	function playingSquareLink(scope, element) {
+		var el = element[0];
 
+		el.droppable = true;
+
+		el.addEventListener("dragenter", function(evt) {
+			// ensure not empty
+			if ( el.hasChildNodes() ) {
+				this.classList.add("warn");
+			} else {
+				this.classList.add("over");
+			}
+			
+			return false;
+		}, false);
+
+		el.addEventListener("dragover", function(evt) {
+			evt.dataTransfer.dropEffect = "move";
+			if ( el.hasChildNodes() ) {
+				this.classList.add("warn");
+			} else {
+				this.classList.add("over");
+			}
+
+			if (evt.preventDefault) evt.preventDefault();
+			return false;
+		}, false);
+
+
+		el.addEventListener("drop", function(evt) {
+			let data         = JSON.parse(evt.dataTransfer.getData("text/plain"));
+			let gamePiece    = document.getElementById(data.gamePieceId);
+			let sourceSquare = document.getElementById(data.sourceId);
+			
+			if (evt.stopPropagation) evt.stopPropagation();
+			if (evt.preventDefault) evt.preventDefault();
+
+			evt.dataTransfer.dropEffect = "move";
+
+			this.classList.remove("over");
+
+			// ensure not empty
+			if ( !el.hasChildNodes() ) {
+
+
+				this.appendChild(gamePiece);
+
+				// is now king?
+				if (squarePosition.isKingsRow(el.id)) {
+					gamePiece.classList.add("king");				// FIXME: move to gamePiece
+				}
+			}
+			
+			
+			this.classList.remove("warn"); // why is this necessary?
+
+			return false;
+		}, false);
+
+
+		["dragleave", "dragend", "dragexit"].forEach(function(event) {
+			el.addEventListener(event, function(evt) {
+				this.classList.remove("over");
+				this.classList.remove("warn");
+				return false;
+			}, false);
+		});
 	}
 
 	return {
